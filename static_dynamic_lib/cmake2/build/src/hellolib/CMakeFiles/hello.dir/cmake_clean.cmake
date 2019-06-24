58dd27259889145f64e96853fba88454e0bbaf12
wisehead <alex.chenhui@gmail.com>
2019-06-24 16:00:30 +0800

    cmake2 dynamic lib ok.

---
+++ static_dynamic_lib/cmake2/build/src/hellolib/CMakeFiles/hello.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello.dir/src/hello.cpp.o"
+  "../../../usr/lib/libhello.pdb"
+  "../../../usr/lib/libhello.dylib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ static_dynamic_lib/cmake2/src/hellolib/src/hello.cpp
@@ -0,0 +1,12 @@
+/*******************************************************************************
+ *      file name: hello.cpp                                               
+ *         author: Hui Chen. (c) 2019                             
+ *           mail: chenhui13@baidu.com                                        
+ *   created time: 2019/06/24-14:58:45                              
+ *  modified time: 2019/06/24-14:58:45                              
+ *******************************************************************************/
+#include "hello.h"
+using namespace std;
+void HelloFunc(){
+    cout<<"Hello My Linux !"<<endl;
+}
