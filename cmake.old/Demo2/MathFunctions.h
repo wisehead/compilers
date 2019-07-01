23291a30bb207a8fdb72102464a9b074cb1891b1
wisehead <alex.chenhui@gmail.com>
2019-07-01 13:08:38 +0800

    add readme and remove cmake.old.

--- cmake.old/Demo2/MathFunctions.h
+++
@@ -1,8 +0,0 @@
-/*******************************************************************************
- *      File Name: MathFunctions.h                                               
- *         Author: Hui Chen. (c) 2016                             
- *           Mail: chenhui13@baidu.com                                        
- *   Created Time: 2016/04/12-16:00                                    
- * Modified Time: 2016/04/12-16:00                                    
- *******************************************************************************/
-double power(double base, int exponent);
