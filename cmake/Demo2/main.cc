286801dd4a3cec71d988a19e923ccfcf18a487d3
wisehead <alex.chenhui@gmail.com>
2019-07-01 11:00:26 +0800

    Demo2 build ok.

--- cmake/Demo2/main.cc
+++ cmake/Demo2/main.cc
@@ -1,28 +1,6 @@
+#include "MathFunctions.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-/**
- * power - Calculate the power of number.
- * @param base: Base value.
- * @param exponent: Exponent value.
- *
- * @return base raised to the power exponent.
- */
-double power(double base, int exponent)
-{
-    int result = base;
-    int i;
-
-    if (exponent == 0) {
-        return 1;
-    }
-    
-    for(i = 1; i < exponent; ++i){
-        result = result * base;
-    }
-
-    return result;
-}
 
 int main(int argc, char *argv[])
 {
