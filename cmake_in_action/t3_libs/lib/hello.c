2dca8199b3cc0d2f9c7f7608279ba097e3b3442c
wisehead <alex.chenhui@gmail.com>
2019-11-07 07:29:25 +0800

    t3 cmake libs

---
+++ cmake_in_action/t3_libs/lib/hello.c
@@ -0,0 +1,12 @@
+/*******************************************************************************
+ *      file name: hello.c                                               
+ *         author: Hui Chen. (c) 2019                             
+ *           mail: chenhui13@baidu.com                                        
+ *   created time: 2019/11/06-16:03:54                              
+ *  modified time: 2019/11/06-16:03:54                              
+ *******************************************************************************/
+#include "hello.h"
+void HelloFunc()
+{
+     printf("Hello World\n");
+}
