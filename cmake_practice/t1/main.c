418ccc2a4d06e7925c24f7e7a4bfd8ead84990f1
wisehead <alex.chenhui@gmail.com>
2019-11-06 11:23:16 +0800

    cmake t1

---
+++ cmake_practice/t1/main.c
@@ -0,0 +1,13 @@
+/*******************************************************************************
+ *      file name: main.c                                               
+ *         author: Hui Chen. (c) 2019                             
+ *           mail: chenhui13@baidu.com                                        
+ *   created time: 2019/11/06-10:50:33                              
+ *  modified time: 2019/11/06-10:50:33                              
+ *******************************************************************************/
+#include <stdio.h>
+int main()
+{
+     printf("Hello World from t1 Main!\n");
+     return 0;
+}
