57bb2c54b7edf2e64e8eb0309d1081663da879c4
alexhuichen <alexhuichen@tencent.com>
2021-01-23 18:38:27 +0800

    pre process

---
+++ cpp_preprocess/hello.c
@@ -0,0 +1,13 @@
+/*******************************************************************************
+ *      File Name: hello.c                                               
+ *         Author: Hui Chen. (c) 2021                             
+ *           Mail: chenhui13@baidu.com                                        
+ *   Created Time: 2021/01/23-18:35                                    
+ * Modified Time: 2021/01/23-18:35                                    
+ *******************************************************************************/
+#include <stdio.h>
+int main() {
+	printf("Hello World!");
+	return 0;
+}
+
