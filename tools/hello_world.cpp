f9ac24e7e119cab15279fd1646fc5ba15599d3ce
wisehead <alex.chenhui@gmail.com>
2021-01-25 10:20:50 +0800

    c++filt

---
+++ tools/hello_world.cpp
@@ -0,0 +1,16 @@
+/*******************************************************************************
+ *      File Name: hello_world.cpp                                               
+ *         Author: Hui Chen. (c) 2021                             
+ *           Mail: chenhui13@baidu.com                                        
+ *   Created Time: 2021/01/25-10:08                                    
+ * Modified Time: 2021/01/25-10:08                                    
+ *******************************************************************************/
+#include <iostream>
+//using namespace std;
+
+int yyyyyy = 100;
+int main() {
+	int xxxxxx = 100;
+	std::cout<<"x is:"<<xxxxxx<<std::endl;
+	std::cout<<"y is:"<<yyyyyy<<std::endl;
+}
