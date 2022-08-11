5681f0a397b6feeb0203490fd53cd080537f6a28
wisehead <alex.chenhui@gmail.com>
2022-08-11 21:50:07 +0800

    subdirectory

---
+++ 02.cmake/parallel101_course/01/10/build/CMakeFiles/a.out.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/chenhui/compilers/02.cmake/parallel101_course/01/10/main.cpp" "/home/chenhui/compilers/02.cmake/parallel101_course/01/10/build/CMakeFiles/a.out.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/chenhui/compilers/02.cmake/parallel101_course/01/10/build/hellolib/CMakeFiles/hellolib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 02.cmake/parallel101_course/01/10/build/hellolib/CMakeFiles/hellolib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hellolib.dir/hello.cpp.o"
+  "libhellolib.a"
+  "libhellolib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hellolib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
