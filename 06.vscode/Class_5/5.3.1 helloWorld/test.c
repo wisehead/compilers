af94c5fe4261900371e0bb226937ddb44d8b125c
wisehead <alex.chenhui@gmail.com>
2022-08-11 10:26:25 +0800

    new CommandLineTool

---
+++ 06.vscode/Class_5/5.3.1 helloWorld/test.c
@@ -0,0 +1,11 @@
+/*******************************************************************************
+ *      file name: test.c                                               
+ *         author: hui chen. (c) 22                             
+ *           mail: alex.chenhui@gmail.com                                        
+ *   created time: 22/08/11- 8:08                                    
+ * modified time: 22/08/11- 8:08                                    
+ *******************************************************************************/
+#include <stdio.h>
+int main() {
+	printf("hello world\n");
+}
