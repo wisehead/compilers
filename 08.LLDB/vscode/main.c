96a2e8c59b1ea977b41a4c89ccc75fae166b4447
wisehead <alex.chenhui@gmail.com>
2022-08-11 17:14:24 +0800

    lldb command line

---
+++ 08.LLDB/vscode/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CLLDB
+//
+//  Created by wisehead on 2022/8/11.
+//
+
+#include <stdio.h>
+
+int main() {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
