186be8528b9b6a4a3bb4c3446c506f4645a4b201
wisehead <alex.chenhui@gmail.com>
2022-08-11 16:39:20 +0800

    CLLDB init

---
+++ 08.LLDB/CLLDB/CLLDB/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CLLDB
+//
+//  Created by wisehead on 2022/8/11.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
